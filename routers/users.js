d66e79678e7871a25bac1b9b9005cc3c17211c44
Nurudeen Ahmed <86122181+nurudeen06@users.noreply.github.com>
2022-11-03 13:17:53 +0100

    Update users.js

--- routers/users.js
+++ routers/users.js
@@ -67,24 +67,24 @@ router.post('/login', async (req,res) => {
         res.status(400).send('Password is wrong');
     }
     router.post('/register', async (req,res)=>{
-        let user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            passwordHash: bcrypt.hashSync(req.body.password, 10),
-            phone: req.body.phone,
-            isAdmin: req.body.isAdmin,
-            street: req.body.street,
-            apartment: req.body.apartment,
-            zip: req.body.zip,
-            city: req.body.city,
-            country: req.body.country,
-        })
-        user = await user.save();
+        // let user = new User({
+        //     name: req.body.name,
+        //     email: req.body.email,
+        //     passwordHash: bcrypt.hashSync(req.body.password, 10),
+        //     phone: req.body.phone,
+        //     isAdmin: req.body.isAdmin,
+        //     street: req.body.street,
+        //     apartment: req.body.apartment,
+        //     zip: req.body.zip,
+        //     city: req.body.city,
+        //     country: req.body.country,
+        // })
+        // user = await user.save();
     
-        if(!user)
+        // if(!user)
         return res.status(400).send('the user cannot be created!')
     
-        res.send(user);
+        //res.send(user);
     })
 
     router.get(`/get/count`, async (req, res) =>{
